a5d7f097cfa22ed103692e735c907a60ff6029cd
kingslayer <dnml39@gmail.com>
2015-02-21 10:46:02 -0700

    Delete authentication_steps.rb

--- features/step_definitions/authentication_steps.rb
+++
@@ -1,6 +0,0 @@
-Given(/^I log in with valid credentials$/) do
-  visit('http://testurlpizza.net:8080/')
-  fill_in('Email', with: "testuser@testuser.com")
-  fill_in('Password', with: "Password1")
-  click_button('Login')
-end
